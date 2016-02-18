c8e2c3291d91c26d293031b7db16895b477dc519
hinzehaley <hinzehaley@gmail.com>
2016-02-17 22:06:05 -0600

    added next button to keyboard when filling out text entries on add memories screen

--- app/src/main/java/carleton150/edu/carleton/carleton150/ExtraFragments/AddMemoryFragment.java
+++ app/src/main/java/carleton150/edu/carleton/carleton150/ExtraFragments/AddMemoryFragment.java
@@ -14,6 +14,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Base64;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -86,6 +87,7 @@ public class AddMemoryFragment extends Fragment {
         EditText etTitle = (EditText) v.findViewById(R.id.et_memory_title);
         EditText etDesc = (EditText) v.findViewById(R.id.et_memory_description);
         EditText etUploader = (EditText) v.findViewById(R.id.et_memory_uploader);
+
         editTexts.add(etTitle);
         editTexts.add(etDesc);
         editTexts.add(etUploader);
